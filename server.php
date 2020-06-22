be6247be4ee26bfa8884f8c38abd77f435872411
Juan Francisco <contrerasjf.0@hotmail.com>
2020-06-22 16:36:32 -0500

    :sparkles: feature(server): Implement the response to the GET verb
    Resolve: C-06

--- server.php
+++ server.php
@@ -16,11 +16,60 @@ if(! in_array($resourceType, $allowedResourceTypes)){
   die;
 }
 
+// Defining resources: book
+$books = [
+  1 => [
+      'title' => 'A Journey to the Center of the Earth',
+      'author' => [
+          'authorId' => 145,
+          'authorName' => 'Jules Verne'
+      ],
+      'genre' => [
+          'genreId' => 1,
+          'genreName' => 'Science Fiction'
+      ],
+  ],
+  2 => [
+      'title' => 'From the Earth to the Moon',
+      'author' => [
+          'authorId' => 145,
+          'authorName' => 'Jules Verne'
+      ],
+      'genre' => [
+          'genreId' => 1,
+          'genreName' => 'Science Fiction'
+      ],
+  ],
+  3 => [
+      'title' => 'Twenty Thousand Leagues Under the Sea',
+      'author' => [
+          'authorId' => 145,
+          'authorName' => 'Jules Verne'
+      ],
+      'genre' => [
+          'genreId' => 1,
+          'genreName' => 'Science Fiction'
+      ],
+  ],
+  4 => [
+      'title' => 'The Mysterious Island',
+      'author' => [
+          'authorId' => 145,
+          'authorName' => 'Jules Verne'
+      ],
+      'genre' => [
+          'genreId' => 1,
+          'genreName' => 'Science Fiction'
+      ],
+  ],
+];
+
+header('Content-Type: application/json');
 
 // Generating the response asumming that the request is correct
 switch (strtoupper($_SERVER['REQUEST_METHOD']) ) {
   case'GET':
-      
+      echo json_encode($books);
   break;
 
   case'POST':
